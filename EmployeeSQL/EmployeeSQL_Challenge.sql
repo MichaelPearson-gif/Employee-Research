6a18b31d42a10d0b8f1e19f9592f39603c34d895
Michael Pearson <mpearson2516@yahoo.com>
2019-09-16 18:15:44 -0700

    Started SQL code and imported tables.

---
+++ EmployeeSQL/EmployeeSQL_Challenge.sql
@@ -0,0 +1,41 @@
+CREATE TABLE departments (
+	dept_no character varying(4) NOT NULL,
+	dept_name character varying(20) NOT NULL
+);
+
+CREATE TABLE dept_emp (
+	emp_no INTEGER NOT NULL,
+	dept_no CHARACTER VARYING(4) NOT NULL,
+	from_date date DEFAULT ('now'::text)::date NOT NULL,
+	to_date date DEFAULT ('now'::text)::date NOT NULL
+);
+
+CREATE TABLE dept_manager (
+	dept_no CHARACTER VARYING(4) NOT NULL,
+	emp_no INTEGER NOT NULL,
+	from_date date DEFAULT ('now'::text)::date NOT NULL,
+	to_date date DEFAULT ('now'::text)::date NOT NULL
+);
+
+CREATE TABLE employees (
+	emp_no INTEGER NOT NULL,
+	birth_date date DEFAULT ('now'::text)::date NOT NULL,
+	first_name CHARACTER VARYING(45) NOT NULL,
+	last_name CHARACTER VARYING(45) NOT NULL,
+	gender CHARACTER VARYING(1) NOT NULL,
+	hire_date date DEFAULT ('now'::text)::date NOT NULL
+);
+
+CREATE TABLE salaries (
+	emp_no INTEGER NOT NULL,
+	salary INTEGER NOT NULL,
+	from_date date DEFAULT ('now'::text)::date NOT NULL,
+	to_date date DEFAULT ('now'::text)::date NOT NULL
+);
+
+CREATE TABLE titles (
+	emp_no INTEGER NOT NULL,
+	title CHARACTER VARYING(100) NOT NULL,
+	from_date date DEFAULT ('now'::text)::date NOT NULL,
+	to_date date DEFAULT ('now'::text)::date NOT NULL
+);
